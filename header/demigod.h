1295b9fbbae67c870a65e64bcadf4e7341385b0d
remi <remi@duphus.com>
2026-02-17 16:07:03 -0500

    initial commit

---
+++ header/demigod.h
@@ -0,0 +1,6 @@
+#ifndef DEMIGOD_H
+#define DEMIGOD_H
+
+#include <SDL3/SDL.h>
+
+#endif//DEMIGOD_H
---
+++ source/demigod.c
@@ -0,0 +1,27 @@
+#include "demigod.h"
+
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+
+SDL_AppResult SDL_AppInit(void** sdl_app_state, int arguments_count, char** arguments_array) {
+   if (!SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO|SDL_INIT_GAMEPAD))
+      return SDL_APP_FAILURE;
+
+   return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* sdl_app_state, SDL_Event* event) {
+   if (event->type == SDL_EVENT_QUIT)
+      return SDL_APP_SUCCESS;
+
+   return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* sdl_app_state) {
+   SDL_RenderPresent(renderer);
+   return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* sdl_app_state, SDL_AppResult result) {
+}
+
